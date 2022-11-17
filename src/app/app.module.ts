a20cf33f547acfdb176f42905ddc5c739756b7fa
CristianGarridoC <cgarrido1400@gmail.com>
2022-11-16 22:24:25 -0600

    feat: Updated app component/module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Modules
+import {AppRoutingModule} from "./app-routing.module";
+import {PagesModule} from "./pages/pages.module";
+import {AuthModule} from "./auth/auth.module";
+
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagefoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
