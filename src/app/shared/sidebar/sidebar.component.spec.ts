0fb69223402276752aa20dd6bb63383a17c108bf
CristianGarridoC <cgarrido1400@gmail.com>
2022-11-21 12:31:03 -0600

    feat: Added unit test in sidebar.component.ts

---
+++ src/app/shared/sidebar/sidebar.component.spec.ts
@@ -0,0 +1,67 @@
+import {MockBuilder, MockedComponentFixture, MockRender} from "ng-mocks";
+import {SidebarComponent} from "./sidebar.component";
+import {SharedModule} from "../shared.module";
+import {DebugElement} from "@angular/core";
+import {IMenu} from "../../services/Interfaces/IMenu";
+
+describe('SidebarComponent', function () {
+  let fixture: MockedComponentFixture<SidebarComponent>;
+  let component: SidebarComponent;
+  let debugElement: DebugElement;
+
+  beforeEach(() => MockBuilder(SidebarComponent, SharedModule));
+  beforeEach(() => {
+    fixture = MockRender(SidebarComponent);
+    component = fixture.point.componentInstance;
+    debugElement = fixture.debugElement;
+  });
+
+  it('should render target component', function () {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render menu item in the view', function () {
+    const menu:IMenu[] = [
+      {
+        title: 'test title',
+        icon: 'test-icon',
+        subMenuItems: []
+      }
+    ];
+    component["menuItems"] = menu;
+    fixture.detectChanges();
+    const menuItems = debugElement.nativeElement.querySelectorAll('.menuItems');
+    const icon = menuItems[0].querySelector('i');
+    const title = menuItems[0].querySelector('.menu-item-title');
+    const itemsCount = menuItems[0].querySelector('.menu-items-count');
+
+    expect(menuItems.length).toBe(1);
+    expect(icon.className).toBe('test-icon');
+    expect(title.textContent).toContain('test title');
+    expect(itemsCount.textContent).toBe('0');
+  });
+
+  it('should render submenu item in the view', function () {
+    const menu:IMenu[] = [
+      {
+        title: 'test',
+        icon: 'test',
+        subMenuItems: [
+          {
+            title: 'submenu 1',
+            path: '/'
+          }
+        ]
+      }
+    ];
+    component["menuItems"] = menu;
+    fixture.detectChanges();
+    const subMenu = debugElement.nativeElement.querySelector('.menuItems > ul');
+    const li_Items = subMenu.querySelectorAll('li');
+    const a_item = li_Items[0].querySelector('a');
+
+    expect(li_Items.length).toBe(1);
+    expect(a_item.textContent).toContain('submenu 1');
+  });
+
+});
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {SidebarService} from "../../services/sidebar.service";
+import {IMenu} from "../../services/Interfaces/IMenu";
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent implements OnInit {
-  protected menuItems: any[];
+  protected menuItems: IMenu[];
 
   constructor(private readonly sidebarService: SidebarService) {
     this.menuItems = sidebarService.menuItems;
-
   }
 
   ngOnInit(): void {
