16df6c52bfbac4cb477b587f681d4663b977a39a
CristianGarridoC <cgarrido1400@gmail.com>
2023-03-10 13:50:56 -0600

    feat: Added backend communication in login component.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import Swal from "sweetalert2";
+
+import {UserService} from "../../services/user.service";
+import {LoginRequest} from "../../interfaces/login-request";
 
 @Component({
   selector: 'app-login',
@@ -7,14 +12,45 @@ import {Router} from "@angular/router";
   styleUrls: [ 'login.component.css' ]
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private readonly router: Router) { }
+  loginForm!: FormGroup;
+  loginRequest!: LoginRequest;
+  constructor(
+    private readonly router: Router,
+    private readonly fb: FormBuilder,
+    private readonly userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      rememberMe: [false]
+    });
   }
 
-  protected login(): void {
-    this.router.navigateByUrl('/');
+  login(): void {
+    if(this.loginForm.invalid){
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    this.loginRequest = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    }
+    this.userService.logIn(this.loginRequest).subscribe({
+      next: response => {
+        console.log('login response', response);
+        this.router.navigateByUrl('/');
+      },
+      error: err => {
+        Swal.fire({
+          title: 'Error!',
+          text: err.error.detail,
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        });
+      }
+    });
   }
 
 }
