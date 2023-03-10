dc39d7d2e36d855a3d42697d91cd9c89f9d33a84
CristianGarridoC <cgarrido1400@gmail.com>
2023-03-10 13:51:13 -0600

    feat: Added backend communication in register component.

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../services/user.service";
+import {SignupRequest} from "../../interfaces/signup-request";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  signupForm!: FormGroup;
+  signupRequest!: SignupRequest;
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(3)]],
+      agreeToTerms: [false, Validators.requiredTrue]
+    }, {validators: this.passwordsMatch});
+  }
+
+  passwordsMatch(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+    if(password?.value === confirmPassword?.value) {
+      if(confirmPassword?.hasError('mismatch')){
+        delete confirmPassword?.errors?.['mismatch'];
+      }
+      confirmPassword?.setErrors(confirmPassword?.errors);
+    }
+    else {
+      confirmPassword?.setErrors({...confirmPassword?.errors, mismatch: true});
+    }
+  }
+
+  signup() {
+    if(this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
+
+    this.signupRequest = {
+      email: this.signupForm.value.email,
+      password: this.signupForm.value.password
+    }
+
+    this.userService.signUp(this.signupRequest).subscribe({
+      next: (response) => {
+        this.signupForm.reset();
+        console.log(response);
+      },
+      error: (error) => {
+        Swal.fire({
+          title: 'Error!',
+          text: error.error.detail,
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        });
+      }
+    });
   }
 
 }
