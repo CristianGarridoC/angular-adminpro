c057e6621d9c27a63d2442c2ed75c5149e68e62f
CristianGarridoC <cgarrido1400@gmail.com>
2023-03-10 14:01:32 -0600

    fix: Changed register to Signup route

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -5,7 +5,7 @@ import {RegisterComponent} from "./register/register.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'signup', component: RegisterComponent }
 ];
 
 @NgModule({
