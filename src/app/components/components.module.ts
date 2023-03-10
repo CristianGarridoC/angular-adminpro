9818c1986dcec6822986b49958ab03614d736a8a
CristianGarridoC <cgarrido1400@gmail.com>
2023-03-10 14:03:11 -0600

    feat: Added InputPasswordWithVisibilityComponent

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {IncreaserComponent} from "./increaser/increaser.component";
-
+import {
+  InputPasswordWithVisibilityComponent
+} from "./input-password-with-visibility/input-password-with-visibility.component";
 
 
 @NgModule({
   declarations: [
-    IncreaserComponent
+    IncreaserComponent,
+    InputPasswordWithVisibilityComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
-    IncreaserComponent
+    IncreaserComponent,
+    InputPasswordWithVisibilityComponent
   ]
 })
 export class ComponentsModule { }
